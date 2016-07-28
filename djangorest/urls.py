22fb823bef43b834ca09a35ab96ff900b341fafb
niyoufa <1061794187@qq.com>
2016-07-28 18:26:11 +0800

    fix code

--- djangorest/urls.py
+++ djangorest/urls.py
@@ -10,10 +10,10 @@ router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+# urlpatterns = [
+#     url(r'^', include(router.urls)),
+#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
 
 urlpatterns = [
     url(r'^snippets', include('snippets.urls')),
--- snippets/urls.py
+++ snippets/urls.py
@@ -5,7 +5,7 @@ from snippets import views
 
 urlpatterns = [
     url(r'/snippets/$', views.SnippetList.as_view()),
-    url(r'/snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'/snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 from rest_framework.urlpatterns import format_suffix_patterns
